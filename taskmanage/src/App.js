8e4467886f2d312e608e4d30de6a60570ed33751
kishore kumar p <65054959+kishorekumar2217@users.noreply.github.com>
2021-03-01 20:27:33 +0530

    Update App.js

--- taskmanage/src/App.js
+++ taskmanage/src/App.js
@@ -1,46 +1,16 @@
 import React from 'react';
-
 import './App.css';
-
-
 import List from './list';
-// import Screen from './screen';
+
 
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-  text:'',
-
-   
-       
-        showText:false,
-      todoChange:false,
-      
-    }
-
-  
-  
-  
-    
-  }
- 
-  
-
-
     
  render(){
   return (
     <div className="App">
-  
-
-{/* <Screen onChange={()=>{this.setState({todoChange:!this.state.todoChange})}}/> */}
-{/* todoChange={this.state.todoChange} */}
    <List/>
-        
-   
-    </div>
+     </div>
   );
  }
 }
